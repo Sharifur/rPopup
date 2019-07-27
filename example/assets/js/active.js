959c407b65632f3a09a4c71e2451a6c609485cc3
Sharifur <robinmedia7@gmail.com>
2019-07-27 12:51:03 +0600

    initial release

---
+++ example/assets/js/active.js
@@ -0,0 +1,24 @@
+(function($) {
+    'use strict';
+
+    $('.image-popup').rPopup({
+        'image': true
+    });
+
+    $('.youtube-popup').rPopup({
+        'video': {
+            embed: true,
+            autoplay: true,
+        },
+    });
+    $('.hosted-popup').rPopup({
+        'video': {
+            embed: true,
+            autoplay: true,
+        },
+    });
+
+
+
+
+})(jQuery);
